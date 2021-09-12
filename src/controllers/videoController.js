fcb073bd7741101f442f02f721dd1678668c8350
ging <minju1348@gmail.com>
2021-09-13 06:59:00 +0900

    Create of CRUD in project

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,61 +1,39 @@
-let videos = [
-  {
-    title: "First video",
-    rating: 5,
-    commnets: 2,
-    createdAt: "2 minutes ago",
-    views: 1,
-    id: 1,
-  },
-  {
-    title: "Second video",
-    rating: 5,
-    commnets: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 2,
-  },
-  {
-    title: "Third video",
-    rating: 5,
-    commnets: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 3,
-  },
-];
-export const trending = (req, res) => {
+import Video from "../models/Video";
+
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  console.log(videos);
   res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+  return res.render("watch", { pageTitle: `Watching` });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+  return res.render("edit", { pageTitle: `Editing` });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
-  const { title } = req.body;
-  const newVideo = {
-    title,
-    rating: 0,
-    commnets: 0,
-    createdAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
-  return res.redirect("/");
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMesssage: error._message,
+    });
+  }
 };
--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
-  useUnifiedTopology: true,
+  useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
