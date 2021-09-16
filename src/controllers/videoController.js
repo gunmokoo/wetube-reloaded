cbe406a8eba3f170a23ba0336a64bc3e0b52021a
ging <minju1348@gmail.com>
2021-09-17 03:02:55 +0900

    Delete of CRUD in project

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,6 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
-  console.log(videos);
   res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = async (req, res) => {
@@ -31,11 +30,8 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags
-      .split(",")
-      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    hashtags: Video.formatHashtags(hashtags),
   });
-  await video.save();
   return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
@@ -47,9 +43,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags
-        .split(",")
-        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
@@ -58,4 +52,10 @@ export const postUpload = async (req, res) => {
       errorMesssage: error._message,
     });
   }
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
 };
--- src/db.js
+++ src/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
 mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useNewUrlParser: true, // it is not necessary to mongoose 6.x ver
+  useUnifiedTopology: true, // it is not necessary to mongoose 6.x ver
+  // useFindAndModify: false, it is not necessary to mongoose 6.x ver
 });
 
 const db = mongoose.connection;
