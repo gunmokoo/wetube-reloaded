f4e00e22bd190bf110d8c5d9657bfc4e122a5d00
ging <minju1348@gmail.com>
2021-09-20 23:57:03 +0900

    edit-profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -137,7 +137,31 @@ export const logout = (req, res) => {
   return res.redirect("/");
 };
 
-export const edit = (req, res) => res.send("Edit User");
+export const getEdit = (req, res) => {
+  return res.render("edit-profile", {
+    pageTitle: "Edit profile",
+  });
+};
+export const postEdit = async (req, res) => {
+  const {
+    session: {
+      user: { _id },
+    },
+    body: { name, email, username, location },
+  } = req;
+  const updateUser = await User.findByIdAndUpdate(
+    _id,
+    {
+      name,
+      email,
+      username,
+      location,
+    },
+    { new: true }
+  );
+  req.session.user = updateUser;
+  return res.redirect("/users/edit");
+};
 export const see = (req, res) => {
   return res.send(`See User #${req.params.id}`);
 };
