dadbf8d6c0ee7d39e8325864bfc077b40f69932e
ging <minju1348@gmail.com>
2021-09-28 05:09:42 +0900

    added flash messages

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -25,6 +25,7 @@ export const getEdit = async (req, res) => {
     return res.status("404").render("404", { pageTitle: "Video not found." });
   }
   if (String(video.owner) !== _id) {
+    req.flash("error", "You are not the owner of  the video");
     return res.status(403).redirect("/");
   }
   return res.render("edit", { pageTitle: `Editing`, video });
@@ -47,6 +48,7 @@ export const postEdit = async (req, res) => {
     description,
     hashtags: Video.formatHashtags(hashtags),
   });
+  req.flash("success", "Changes saved.");
   return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
