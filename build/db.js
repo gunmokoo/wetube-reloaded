022e59a58006937fb9c69dd837c4885d87db20e0
ging <minju1348@gmail.com>
2021-10-01 21:06:30 +0900

    building the Backend on server

---
+++ build/db.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var _mongoose = _interopRequireDefault(require("mongoose"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_mongoose["default"].connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  // it is not necessary to mongoose 6.x ver
+  useUnifiedTopology: true // it is not necessary to mongoose 6.x ver
+  // useFindAndModify: false, it is not necessary to mongoose 6.x ver
+  // useCreateIndex: true
+
+});
+
+var db = _mongoose["default"].connection;
+
+var handleOpen = function handleOpen() {
+  return console.log("Connected to DB");
+};
+
+var handleError = function handleError(error) {
+  return console.log("DB Error", error);
+};
+
+db.on("error", handleError);
+db.once("open", handleOpen);
